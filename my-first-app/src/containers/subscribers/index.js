d639de89e090a6ff50be9a605879380ecec62625
LjubanDragosavljevic <ljubandragosavljevic@gmail.com>
2020-10-13 17:32:09 +0200

    2020-10-13

--- my-first-app/src/containers/subscribers/index.js
+++ my-first-app/src/containers/subscribers/index.js
@@ -10,8 +10,8 @@ function Subscribers() {
                 <a href="#subscribers"><img src="images/Shape-3.png" alt="" className='icon-nav'/>Subscribers</a>
                 <a href="#networks"><img src="images/Shape-4.png" alt="" className='icon-nav'/>Networks</a>
                 <a href="#ispplans"><img src="images/Shape-5.png" alt="" className='icon-nav'/>ISP Plans</a>
-                <a href="#taskmanager"><img src="images/Shape-1.png" alt="" className='icon-nav'/>Task Manager</a>
-                <a href="#scriptlibrary"><img src="images/Shape-2.png" alt="" className='icon-nav'/>Script Library</a>
+                <a href="#taskmanager"><img src="images/Shape-2.png" alt="" className='icon-nav'/>Task Manager</a>
+                <a href="#scriptlibrary"><img src="images/Shape-1.png" alt="" className='icon-nav'/>Script Library</a>
                 <a href="#preferences"><img src="images/Shape-6.png" alt="" className='icon-nav'/>Preferences</a> 
                 <a className="bottomBorder" href="#system"><img src="images/SYSTEM.png" alt="" className='system'/></a>
                 <a href="#accounts"><img src="images/user.png" alt="" className='icon-nav'/>Accounts</a>
